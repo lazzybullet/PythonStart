3072f64bc4baba9b2d805983c598bbe47fe3c96a
lazzy <lazzybullet@gmail.com>
2022-08-29 21:12:07 +0300

    2-nd lesson finish

--- lesson_2.py
+++ lesson_2.py
@@ -1,14 +1,61 @@
 # print(1 + 2)
 #  Ctrl + ?
+
 value_1 = -16 # int
-value_2 = 5.2 #float
+value_2 = 0.5 #float
+# # result = value_1 * value_2
+# # result = value_1 / value_2
+# # result = value_1 // value_2 # целочисленное деление
+# # result = value_1 % value_2 # остаток от  деления
+# result = value_1 ** value_2 # ** - степень
+# print(result)
 
-# result = value_1 * value_2
-# result = value_1 / value_2
-# result = value_1 // value_2 # целочисленное деление
-# result = value_1 % value_2 # остаток от  деления
-result = value_1 ** value_2 # ** - степень
 
+# a = 2
+# b = 3
+# c = 4
+# D = b ** 2 - 4 * a * c
 
+# active_users_count = 1000
+# price = 10
+#
+# total_money = active_users_count * price
+# print(total_money)
 
-print(result)
+# PascalCaseObjectNotification
+# camelCaseObjectNotification
+# cebab-object-notification
+
+# RATIO = 100
+
+# print(total_money, RATIO)
+
+# PEP-8 formatting Alt+Ctrl + L
+
+# value_type = type(value_2)
+# print(value_2, value_type)
+
+# value = 123123123
+# new_value = value
+# print(id(value), id(new_value))
+
+value = "qwe" #  "" тип данных строка
+# print(value, type(value))
+my_str = "qw"
+# result = value + " " + value # конкатенация
+# result = value - my_str
+
+# result = value * 5
+#
+#
+# print(result)
+
+############################### Литералы строк ##############################
+
+my_str = "New_string"
+my_str = 'New_string'
+my_str = """New string"""
+my_str = '''New string'''
+
+my str = 'FC "Arsenal"'
+
