048371f6286528b245282745b1fa9b61f8b9e414
lazzy <lazzybullet@gmail.com>
2022-08-29 20:09:21 +0300

    2-nd lesson

---
+++ lesson_2.py
@@ -0,0 +1,14 @@
+# print(1 + 2)
+#  Ctrl + ?
+value_1 = -16 # int
+value_2 = 5.2 #float
+
+# result = value_1 * value_2
+# result = value_1 / value_2
+# result = value_1 // value_2 # целочисленное деление
+# result = value_1 % value_2 # остаток от  деления
+result = value_1 ** value_2 # ** - степень
+
+
+
+print(result)
